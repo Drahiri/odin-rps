ae0a68725a6166c45e3f272c5a383d533a7e6c3c
Drahiri <kodzidig@o2.pl>
2026-05-01 18:01:16 +0200

    Play a round

--- script.js
+++ script.js
@@ -20,3 +20,23 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+    if (
+        humanChoice === "rock" && computerChoice === "scissors"
+        || humanChoice === "paper" && computerChoice === "rock"
+        || humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+        humanScore++;
+        console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+    } else {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
