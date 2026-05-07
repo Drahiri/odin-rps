0f99ef33b77bbf6ce79fcb6fa4dccf7ed5bf823c
Drahiri <kodzidig@o2.pl>
2026-05-07 09:01:30 +0200

    Remove unneeded code

--- script.js
+++ script.js
@@ -14,11 +14,6 @@ function getComputerChoice() {
 }
 
 
-function getHumanChoice() {
-    return prompt("Choose one: rock, paper, scissors").toLowerCase();
-}
-
-
 function capitalize(text) {
     return text[0].toUpperCase() + text.slice(1);
 }
@@ -47,30 +42,6 @@ function playRound(humanChoice, computerChoice) {
 let humanScore = 0;
 let computerScore = 0;
 const NUMBER_OF_ROUNDS = 5;
-
-function playGame() {
-
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        const winner = playRound(humanSelection, computerSelection);
-
-        switch (winner) {
-            case "human":
-                humanScore++;
-                break;
-
-            case "computer":
-                computerScore++;
-                break;
-        }
-    }
-
-    console.log("Final scores:")
-    console.log(`Player: ${humanScore}`);
-    console.log(`Computer: ${computerScore}`);
-}
 
 function updateScore(winner) {
     const scores = document.querySelector("#scores");
